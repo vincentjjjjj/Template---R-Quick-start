02159cf651527dc6eae591aab3fb6a03e91c77d9
vincentjjjjj <44926203+vincentjjjjj@users.noreply.github.com>
2022-09-23 08:48:17 +0400

    Update template.r

--- template.r
+++ template.r
@@ -9,6 +9,28 @@ output: html_document
 # options(encoding = "UTF-8", warn = -1, "openxlsx.dateFormat" = "dd/mm/yyyy")
 options(encoding = "UTF-8", "openxlsx.dateFormat" = "dd/mm/yyyy", xlsx.date.format = "dd/mm/yyyy", DT.warn.size = FALSE, readxl.show_progress = T)
 
+
+if (F) {
+  input  = basename(rstudioapi::getSourceEditorContext()$path)
+  output = gsub(".Rmd", ".R", input)
+  knitr::purl(input, output = output)
+  writeLines(as.character(parse(output)), output)
+  
+  # output_bat = gsub(".Rmd", ".bat", input)
+  # writeLines(paste0("@echo off\nRScript.exe --encoding=UTF-8 ", output, "\npause\n"), output_bat)
+}
+
+
+if (!"pacman" %in% rownames(installed.packages())) {
+  print("Installation du programme en cours...")
+  user_libs = Sys.getenv("R_LIBS_USER")
+  dir.create(user_libs, showWarnings = F, recursive = T)
+  install.packages(c("pacman", "hms"), lib = user_libs, repos = "https://cran.rstudio.com/", quiet = T, verbose = F)
+  shell("cls")
+  stop("Installation du programme terminé. Relancer le programme.")
+}
+
+
 pacman::p_load(
   stringi,
   fs,
@@ -49,23 +71,25 @@ write_xlsx <- function(df, filename) {
   cols_period = which(map(df, is.period) == T)
   
   df = df %>% mutate(across(all_of(cols_period), ~ .x %>% hms::hms() %>% str_sub(end = -4)))
-  # df = df %>% mutate(across(everything(), as.character))
+  df = df %>% mutate(across(everything(), as.character))
   df %>% as.data.frame() %>% writexl::write_xlsx(filename)
 }
 
 
 write_csv <- function(df, filename) {
-  filename %>% dirname() %>% dir_create()
+  filename %>% path_dir() %>% dir_create()
+  filename = filename %>% path_ext_set("csv")
   df = df %>% ungroup()
   cols_date   = which(map(df, is.Date) == T)
   cols_period = which(map(df, is.period) == T)
   df = df %>% mutate(across(all_of(cols_period), ~ .x %>% hms::hms() %>% str_sub(end = -4)))
+  df = df %>% mutate(across(everything(), as.character))
   df %>% write_csv(filename)
 }
 
 
-clean_colnames <- function(.data) {
-  .data %>% rename_all(~ .x %>% str_to_lower() %>% str_replace_all(" |/", "_") %>% stri_trans_general("Latin-ASCII") %>% str_remove_all("[^a-zA-Z0-9_]"))
+clean_colnames <- function(.tbl) {
+  .tbl %>% rename_all(~ .x %>% str_to_lower() %>% str_replace_all(" |/", "_") %>% stri_trans_general("Latin-ASCII") %>% str_remove_all("[^a-zA-Z0-9_]"))
 }
 ```
 
